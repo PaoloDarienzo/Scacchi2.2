a94bcd946bf329277e5dadc6651e70d422b71504
Paolo D'Arienzo <Pleasant94@gmail.com>
2015-09-18 15:58:38 +0200

    Updated Javadoc

--- src/view/BarMenu.java
+++ src/view/BarMenu.java
@@ -1,7 +1,6 @@
 
 package view;
 
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,8 +12,11 @@ import javax.swing.JOptionPane;
  */
 public class BarMenu extends MenuBar {
 
-	private static final long serialVersionUID = 1L;
-    private final Menu help = new Menu("Help");
+	//TODO
+	//sistemare la javadoc
+	private static final long serialVersionUID = -2397370419829851151L;
+
+	private final Menu help = new Menu("Help");
 
     private final MenuItem about = new MenuItem("About");
     private final MenuItem rules = new MenuItem("Rules");
@@ -34,13 +36,13 @@ public class BarMenu extends MenuBar {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "L’obbiettivo del giocatore è quello di battere l’avversario dando scacco \n"
+                JOptionPane.showMessageDialog(null, "L'obbiettivo del giocatore e' quello di battere l'avversario dando scacco \n"
                 		+ "matto al suo Re. Questo accade quando il Re nemico si trova sotto scacco, \n"
-                		+ "cioè sotto il tiro di uno o più pezzi del giocatore, e non gli è possibile \n"
-                		+ "spostarsi in altre case, perché bloccate o anch’esse sotto scacco, o parare \n"
+                		+ "cioe' sotto il tiro di uno o piu' pezzi del giocatore, e non gli e' possibile \n"
+                		+ "spostarsi in altre caselle, perche' bloccate o anch'esse sotto scacco, o bloccare \n"
                 		+ "lo scacco in altra maniera, come per esempio mangiando il pezzo che sta dando \n"
-                		+ "scacco con una qualunque figura del proprio schieramento o interponendo una di \n"
-                		+ "esse lungo la linea del pezzo che gli sta dando scacco. \n", "Rules", JOptionPane.INFORMATION_MESSAGE);
+                		+ "scacco con un qualunque pezzo del proprio schieramento o interponendo uno di \n"
+                		+ "essi lungo la linea del pezzo che gli sta dando scacco. \n", "Rules", JOptionPane.INFORMATION_MESSAGE);
             }
         });
 
@@ -48,7 +50,7 @@ public class BarMenu extends MenuBar {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null,"                     Created by \n Paolo D'arienzo & Serena Cavaletti", "About", JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(null,"                     Created by \n Paolo D'Arienzo & Serena Cavaletti", "About", JOptionPane.INFORMATION_MESSAGE);
             }
         });
     }
